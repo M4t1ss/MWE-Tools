f736a1e3846f37ea4ee5b43738fcc79b7b24655f
Matīss <matiss@lielakeda.lv>
2017-04-02 22:14:23 +0300

    Forgot how to use this...
    Added a usage example comment

--- MWE translation workflow/align.php
+++ MWE translation workflow/align.php
@@ -1,15 +1,18 @@
 ﻿<?php
 
-//Input files
+//	Usage:
+//	1) Source and target '.taas.tagged' files that are produced by XMLConverter/converter from MWE Tools
+//	2) Remove '<TENAME>' and '</TENAME>' xml tags and everything that's between them (sed can be used)
+//	3) Filter out duplicate sentences so that only uniqe ones are left
+//	4) Feed the files with uniqe source and target sentences (where each line starts with the sentence ID) as inputs for this script
+
 $source 	= $argv[1];
 $target		= $argv[2];
-
 
 $inSRC = fopen($source, "r") or die("Can't open input file!");
 $inTRG = fopen($target, "r") or die("Can't open input file!");
 $outSRC = fopen($source.".out.txt", "a") or die("Can't create output file!");
 $outTRG = fopen($target.".out.txt", "a") or die("Can't create output file!");
-
 
 if ($inSRC && $inTRG) {
     while (($sourceSentence = fgets($inSRC)) !== false) {
